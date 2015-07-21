1388dfadc0936bc862c5b1befb6fccbbcc70a865
Matan Gover <matangover@gmail.com>
2015-07-22 01:56:27 +0300

    Use flask-security to manage users

--- app.py
+++ app.py
@@ -2,77 +2,40 @@ import os
 from flask import Flask, render_template, request, redirect, url_for
 from flask.ext.heroku import Heroku
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.openid import OpenID
-from flask.ext.login import current_user, login_user, LoginManager, logout_user, login_required, UserMixin
+from flask.ext.security import Security, SQLAlchemyUserDatastore, \
+    UserMixin, RoleMixin, login_required
+
 
 app = Flask(__name__)
 heroku = Heroku(app)
 db = SQLAlchemy(app)
-oid = OpenID(app, 'openid_store', safe_roots=[]) # TODO: modify path?
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = "login"
-
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'this_should_be_configured')
+app.config['DEBUG'] = True
+
+#app.config['SECURITY_PASSWORD_HASH'] = "bcrypt"
+#app.config['SECURITY_PASSWORD_SALT'] = "change this to something else"
+
+roles_users = db.Table('roles_users',
+        db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+        db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80))
     email = db.Column(db.String(254), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
 
-    def __init__(self, name, email):
-        self.name = name
-        self.email = email
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
 
-    def __repr__(self):
-        return '<Name %r>' % self.name
-
-
-@login_manager.user_loader
-def load_user(id):
-    return User.query.get(int(id))
-
-@app.route('/login', methods=['GET', 'POST'])
-@oid.loginhandler
-def login():
-    if current_user.is_authenticated():
-        return redirect(url_for('index'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        session['remember_me'] = form.remember_me.data
-        return oid.try_login(form.openid.data, ask_for=["email"])
-
-    return render_template('login.html', form=form, providers=app.config['OPENID_PROVIDERS'])
-
-@oid.after_login
-def after_login(resp):
-    if resp.email is None or resp.email == "":
-        flash('Invalid login. Please try again.')
-        return redirect(url_for('login'))
-    user = User.query.filter_by(email=resp.email).first()
-    if user is None:
-        flash('You are not allowed to log in. Please try again.')
-        return redirect(url_for('login'))
-    remember_me = False
-    if 'remember_me' in session:
-        remember_me = session['remember_me']
-        session.pop('remember_me', None)
-    login_user(user, remember=remember_me)
-    next = request.args.get('next')
-    if not next_is_valid(next):
-        return flask.abort(400)
-
-    return redirect(next or url_for('index'))
-
-@app.route('/logout')
-def logout():
-    logout_user()
-    return redirect(url_for('index'))
-
-###
-# Routing for your application.
-###
 @app.route('/')
 @login_required
 def home():
@@ -86,15 +49,10 @@ def about():
     return render_template('about.html')
 
 
-###
-# The functions below should be applicable to all Flask apps.
-###
-
-@app.route('/<file_name>.txt')
-def send_text_file(file_name):
+@app.route('/robots.txt')
+def send_text_file():
     """Send your static text file."""
-    file_dot_text = file_name + '.txt'
-    return app.send_static_file(file_dot_text)
+    return app.send_static_file("robots.txt")
 
 @app.errorhandler(404)
 def page_not_found(error):
