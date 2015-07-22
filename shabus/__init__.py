a8e5fad39b63386992d5652c34a341c747404d1b
Matan Gover <matangover@gmail.com>
2015-07-23 01:15:02 +0300

    Add bootstrap and make login form prettier

--- shabus/__init__.py
+++ shabus/__init__.py
@@ -3,11 +3,13 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask.ext.heroku import Heroku
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.security import Security, SQLAlchemyUserDatastore
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object('config')
 heroku = Heroku(app)
 db = SQLAlchemy(app)
+Bootstrap(app)
 
 from shabus.models import User, Role
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
