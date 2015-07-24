d0ee48ca38a94eae2133dd4d1b6f594ac32fcb60
Matan Gover <matangover@gmail.com>
2015-07-24 17:11:51 +0300

    Re-arrange code

--- shabus/import_members.py
+++ shabus/import_members.py
@@ -14,19 +14,42 @@ def main():
         reader = unicode_csv_reader(input_file)
         recommending_member_phones = {}
         for member_row in reader:
-            member_id, recommending_member_phone = process_row(member_row)
-            recommending_member_phones[member_id] = recommending_member_phone
+            member, recommending_member_phone = process_row(member_row)
+            recommending_member_phones[member] = recommending_member_phone
 
-    for member_id, recommending_member_phone in recommending_member_phones.items():
+    for member, recommending_member_phone in recommending_member_phones.items():
         if recommending_member_phone == "":
             continue
         recommending_member = db.query.Member.query.get(phone_number=recommending_member_phone)
-        db.query.Member.query.get(member_id).recommending_member = recommending_member
+        member.recommending_member = recommending_member
 
     db.session.commit()
 
 def process_row(member_row):
-    csv_columns = (
+    member_dict = dict(zip(get_csv_columns(), member_row))
+
+    member = db.Member()
+    string_member_attributes = ("age", "email", "desired_ride_time", "desired_board_time",
+        "desired_return_time", "legal_statement", "signature_image_url",
+        "credit_card_payment_products", "credit_card_payer_info", "credit_card_payer_address",
+        "jotform_submission_id", "comments")
+    for string_attribute in string_member_attributes:
+        save_string(member_dict, member, string_attribute)
+
+    address_attribute_prefixes = ("", "additional", "desired_destination")
+    for address_prefix in address_attribute_prefixes:
+        save_address(member_dict, member, address_prefix)
+    bool_attributes = ("backed_on_headstart", "first_of_may", "used_old_form_to_sign_up",
+        "is_founder", "is_manager")
+    for bool_attribute in bool_attributes:
+        save_bool(member_dict, member, bool_attribute)
+    save_datetime(member_dict, member, "submission_date")
+    db.session.add()
+
+    return member, recommending_member_phone_number
+
+def get_csv_columns():
+    return (
         "repeat_check", "submission_date", "age", "last_name", "first_name", "phone_number",
         "has_smartphone", "email", "street", "house_number", "city", "neighborhood", "zipcode",
         "country", "has_additional_address", "additional_address_street", "additional_address_house_number",
@@ -43,28 +66,6 @@ def process_row(member_row):
         "ip_address", "jotform_submission_id", "used_old_form_to_sign_up", "dynamic_support_check",
         "dynamic_support_or_payment_check", "dynamic_is_eligible_for_membership", "is_manager",
         "is_founder", "comments")
-
-    member_dict = dict(zip(csv_columns, member_row))
-
-    member = db.Member()
-    string_member_attributes = ("age", "email", "desired_ride_time", "desired_board_time",
-        "desired_return_time", "legal_statement", "signature_image_url",
-        "credit_card_payment_products", "credit_card_payer_info", "credit_card_payer_address",
-        "jotform_submission_id", "comments")
-    for
-
-    address_attribute_prefixes = ("", "additional", "desired_destination")
-    for address_prefix in address_attribute_prefixes:
-        save_address(member_dict, member, address_prefix)
-    bool_attributes = ("backed_on_headstart", "first_of_may", "used_old_form_to_sign_up",
-        "is_founder", "is_manager")
-    for bool_attribute in bool_attributes:
-        save_bool(member_dict, member, bool_attribute)
-    save_datetime(member_dict, member, "submission_date")
-    db.session.add()
-
-    return recommending_member_phone_number
-
 
 
 if __name__ == "__main__":
