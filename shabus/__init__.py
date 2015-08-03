d34e574010a2248c1c8ce2a7e73c37366e96d07c
Matan Gover <matangover@gmail.com>
2015-08-04 02:00:15 +0300

    Add admin views for all models

--- shabus/__init__.py
+++ shabus/__init__.py
@@ -18,9 +18,14 @@ user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 security = Security(app, user_datastore)
 
 from shabus.admin import ProtectedModelView, ProtectedAdminIndexView
+from shabus.models import Member, Passenger, Ride, Address
 admin = Admin(app, name="shabus", template_mode="bootstrap3", index_view=ProtectedAdminIndexView())
 admin.add_view(ProtectedModelView(Role, db.session))
 admin.add_view(ProtectedModelView(User, db.session))
+admin.add_view(ProtectedModelView(Member, db.session))
+admin.add_view(ProtectedModelView(Passenger, db.session))
+admin.add_view(ProtectedModelView(Ride, db.session))
+admin.add_view(ProtectedModelView(Address, db.session))
 
 import shabus.views
 
